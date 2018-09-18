79af1d8cf22c44c0e29c0e9e3e7e65de530ae653
Carlos Sanchez <csan85@outlook.com>
2018-09-18 14:40:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-numbers1 = [3, 4, 6, 8]
+cities = ["London", "Madrid", "Paris", "Medellin"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-array << element
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
